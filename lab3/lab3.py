bbd6591a1e830a5767b3319935a8325d55bb901f
SuperChen <chenmengyang_2016@hotmail.com>
2025-02-11 20:59:33 +0800

    Lab3 TG Bot

---
+++ lab3/lab3.py
@@ -0,0 +1,27 @@
+import configparser
+import logging
+import telegram
+from telegram.ext import Updater, MessageHandler, Filters
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    dispatcher = updater.dispatcher
+    # Logging
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+    # Register dispatcher to handle message
+    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    dispatcher.add_handler(echo_handler)
+    # Start bot
+    updater.start_polling()
+    updater.idle()
+
+def echo(update, context):
+    reply_message = update.message.text.upper()
+    logging.info("Update: " + str(update))
+    logging.info("Context: " + str(context))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
+
+if __name__ == '__main__':
+    main()
